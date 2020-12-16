350a3568056bc95e0c5e113fba83c3d2c95bcf9a
DK <dkom91@gmail.com>
2020-12-16 08:24:42 +0300

    init master

---
+++ src/main/java/ru/boost/feature/controllers/HealthController.java
@@ -0,0 +1,16 @@
+package ru.boost.feature.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String getHealth() {
+        log.info("I'm alive!!");
+        return "OK";
+    }
+}
